c9fed4e308fc805bb67c380c6febdad50d48a5ea
Andrei Rabusov <a.rabusov@tum.de>
2023-01-08 23:20:42 +0100

    Add primitive unit testing

---
+++ Makefile
@@ -0,0 +1,15 @@
+CC=cc
+CFLAGS=-ansi -pedantic -Wall -Wextra -Werror
+
+all: sudoku
+
+sudoku: sudoku.c
+	@$(CC) $(CFLAGS) -o $@ $^
+	@echo "  [CC]	$@"
+test:
+	@bash test.sh
+clean:
+	@rm -f sudoku
+	@echo "  [RM]	sudoku"
+
+PHONY: all, sudoku, clean, test
---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+CWD=$(pwd)
+if [[ ! -f Makefile ]]; then
+    echo "Run in the repo root directory"
+    exit 1
+fi
+if [[ ! -f ${CWD}/sudoku ]]; then
+    make
+fi
+cd examples
+PASS=0
+TOT=0
+for EX in ex*.txt; do
+    TMP=$(mktemp)
+    ${CWD}/sudoku < $EX > $TMP
+    SOL=sol${EX#ex}
+    DIFF=$(diff $SOL $TMP)
+    rm ${TMP}
+    if [[ "$DIFF" == "" ]]; then
+        let PASS=$PASS+1
+    fi
+    let TOT=$TOT+1
+done
+echo Test result: [$PASS/$TOT]
+if [[ $PASS -eq $TOT ]]; then
+    echo Ok.
+else
+    echo Fail.
+fi
+cd $CWD
